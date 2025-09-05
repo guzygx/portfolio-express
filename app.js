188d525406e17e1457de02094c3ba25f228a037e
Guzy GX <gxuzxu@gmail.com>
2025-09-05 09:49:23 +0200

    livereload browser when frontend change

--- app.js
+++ app.js
@@ -3,38 +3,50 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts');
 var indexRouter = require('./routes/index');
-
-var favicon = require('serve-favicon')
-
+var favicon = require('serve-favicon');
+var livereload = require("livereload");
+var connectLiveReload = require("connect-livereload");
 var app = express()
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
-var app = express();
-
-// view engine setup
+// App setup
+app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(expressLayouts)
-app.set('layout', './layouts/_main')
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(expressLayouts);
+app.set('layout', './layouts/_main');
 
-app.use('/', indexRouter);
+// Setup browser refresh when front-end change (remove this if you use a bundler)
+if (process.env.NODE_ENV === "development") {
+  const liveReloadServer = livereload.createServer({
+    exts: ["css", "js", "ejs"],
+  });
+
+  liveReloadServer.watch([path.join(__dirname, "views"), path.join(__dirname, "public")]);
+  
+  liveReloadServer.server.once("connection", () => {
+    console.log('Connecting to livereload server')
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 10);
+  });
+
+  app.use(connectLiveReload());
+}
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -43,5 +55,8 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', { title: 'Error page' });
 });
+
+// Routes and routers
+app.use('/', indexRouter);
 
 module.exports = app;
